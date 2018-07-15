6935bb9084924f1446f1ed6f2ece2eacac29e5f4
Greggy Gianini F <greggygf@gmail.com>
2018-07-15 14:42:18 +0700

    change mysql to mysqli

--- code/proses/input/input_detail.php
+++ code/proses/input/input_detail.php
@@ -7,9 +7,9 @@ $Dosis = $_POST['Dosis'];
 $Jumlah = $_POST['Jumlah'];
 
 $cariharga = "select HargaObat from `obat` where KodeObat='$KodeObat'";
-$cariharga_sql = mysql_query($cariharga);
+$cariharga_sql = mysqli_query($connect,$cariharga);
 	
-while ($isi = mysql_fetch_array($cariharga_sql))
+while ($isi = mysqli_fetch_array($cariharga_sql))
 {
 	$HargaObat = $isi['HargaObat'];
 }
@@ -18,7 +18,7 @@ $SubTotal = $HargaObat*$Jumlah;
 
 $insertDetail = "INSERT INTO detail(NomorResep,KodeObat,Dosis,Jumlah,SubTotal) values ('$NomorResep','$KodeObat','$Dosis','$Jumlah','$SubTotal')";
 
-$insertDetail_query = mysql_query($insertDetail);
+$insertDetail_query = mysqli_query($connect,$insertDetail);
 
 if ($insertDetail_query)
 {
