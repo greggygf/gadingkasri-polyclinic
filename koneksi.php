6df7753baff3f0dc42eb300897daa4a6a66eca25
Satria Chandra <satriachandrayw@gmail.com>
2021-09-18 21:27:44 +0700

    feat: init composer

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 	$host = "localhost";
-	$user = "root";
-	$password = "";
-	$database = "tebusobat";
+	$user = "cakralaksana";
+	$password = "P@ssw0rd123";
+	$database = "cakralaksana";
 	
 	$connect = mysqli_connect($host,$user,$password,$database) or die (mysqli_error($connect));
 	//mysql_select_db($database) or die (mysql_error());
